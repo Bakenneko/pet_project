3e0aba58d5f48384cd586e2170d64ed1d4092467
Andrew Kurylyk <andronniy7@gmail.com>
2025-06-22 19:31:21 +0300

    19:31

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,14 @@
+import type {FC} from "react";
+
+
+type MyComponentPropType = {text: string};
+
+const MyComponent:FC<MyComponentPropType> = ({text}) => {
+    return (
+        <div>
+            {text}
+        </div>
+    );
+};
+
+export default MyComponent;
